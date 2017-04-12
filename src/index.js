6861028234cf9304fb8d12332f402c87b7d48c62
Tobias Lins <tobias@lins.in>
2017-04-12 14:52:27 +0200

    add etag header

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const url = require('url')
+const crypto = require('crypto')
 const image = require('./image')
 const svgExt = /\.svg$/;
 const pngExt = /\.png$/;
@@ -12,10 +13,11 @@ module.exports = (req, res) => {
     pathname = Math.random().toString()
   } else {
     res.setHeader('Cache-Control', 'max-age=2592000, public')
+    res.setHeader('ETag', crypto.createHash('md5').update(pathname).digest("hex"))
   }
   if (query.type === 'svg' || svgExt.test(pathname)) {
     res.setHeader('Content-Type', 'image/svg+xml')
-    return image.generateSVG(pathname.replace(svgExt, ''), query.size);
+    return image.generateSVG(pathname.replace(svgExt, ''), query.size)
   }
   res.setHeader('Content-Type', 'image/png')
   return image.generatePNG(pathname.replace(pngExt, ''), query.size)
