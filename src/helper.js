7439efc20cdc0a991004981847f3f000f3ea406b
Tobias Lins <tobias@lins.in>
2017-04-15 16:07:34 +0200

    Reformat with prettier

--- src/helper.js
+++ src/helper.js
@@ -3,7 +3,7 @@
 function djb2(str) {
   let hash = 5381
   for (let i = 0; i < str.length; i++) {
-    hash = ((hash << 5) + hash) + str.charCodeAt(i)
+    hash = (hash << 5) + hash + str.charCodeAt(i)
   }
   return hash
 }
@@ -19,10 +19,15 @@ function shouldChangeColor(color) {
 
 function hashStringToColor(str) {
   const hash = djb2(str)
-  const r = (hash & 0xFF0000) >> 16
-  const g = (hash & 0x00FF00) >> 8
-  const b = hash & 0x0000FF
-  return '#' + ('0' + r.toString(16)).substr(-2) + ('0' + g.toString(16)).substr(-2) + ('0' + b.toString(16)).substr(-2)
+  const r = (hash & 0xff0000) >> 16
+  const g = (hash & 0x00ff00) >> 8
+  const b = hash & 0x0000ff
+  return (
+    '#' +
+    ('0' + r.toString(16)).substr(-2) +
+    ('0' + g.toString(16)).substr(-2) +
+    ('0' + b.toString(16)).substr(-2)
+  )
 }
 
 function getMatchingColor(firstColor) {
