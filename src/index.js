5207142484b6eaa60a7076e64decdaf5edf80aa2
ulrichformann <ulrichformann@Ulrichs-MacBook-Pro.local>
2017-07-20 11:58:25 +0200

    size parameter with width and height
    use like size=200x300

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const crypto = require('crypto')
 const image = require('./image')
 const svgExt = /\.svg$/
 const pngExt = /\.png$/
+const sizePat = /^\d+x\d+$/
 
 module.exports = (req, res) => {
   let { pathname, query } = url.parse(req.url, true)
@@ -15,10 +16,17 @@ module.exports = (req, res) => {
     res.setHeader('Cache-Control', 'max-age=2592000, public')
     res.setHeader('Last-Modified', 'Mon, 03 Jan 2011 17:45:57 GMT')
   }
+  let height
+  if (sizePat.test(query.size)) {
+    height = query.size.slice(query.size.indexOf('x') + 1)
+    query.size = query.size.slice(0, query.size.indexOf('x'))
+  } else {
+    height = query.size
+  }
   if (query.type === 'svg' || svgExt.test(pathname)) {
     res.setHeader('Content-Type', 'image/svg+xml')
-    return image.generateSVG(pathname.replace(svgExt, ''), query.text || '', query.size)
+    return image.generateSVG(pathname.replace(svgExt, ''), query.text || '', query.size, height || '')
   }
   res.setHeader('Content-Type', 'image/png')
-  return image.generatePNG(pathname.replace(pngExt, ''), query.size)
+  return image.generatePNG(pathname.replace(pngExt, ''), query.size, height || '')
 }
