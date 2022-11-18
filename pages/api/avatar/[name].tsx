12d4a24ea04067995d710df2aa8cf94fb815a8d8
Tobias Lins <me@tobi.sh>
2022-11-18 12:25:18 +0100

    Migrate to ts and Vercel Edge functions

---
+++ pages/api/avatar/[name].tsx
@@ -0,0 +1,68 @@
+import { ImageResponse } from "@vercel/og";
+import { NextRequest, NextResponse } from "next/server";
+import { renderToReadableStream } from "react-dom/server";
+import { generateGradient } from "../../../utils/gradient";
+
+export const config = {
+  runtime: "experimental-edge",
+};
+
+export default async function (req: NextRequest, res: NextResponse) {
+  const url = new URL(req.url);
+  const name = url.searchParams.get("name");
+  const text = url.searchParams.get("text");
+  const size = Number(url.searchParams.get("size") || "120");
+  const [username, type] = name?.split(".") || [];
+  const fileType = type?.includes("svg") ? "svg" : "png";
+
+  const gradient = generateGradient(username || Math.random() + "");
+
+  const avatar = (
+    <svg
+      width={size}
+      height={size}
+      viewBox={`0 0 ${size} ${size}`}
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <defs>
+          <linearGradient id="gradient" x1="0" y1="0" x2="1" y2="1">
+            <stop offset="0%" stop-color={gradient.fromColor} />
+            <stop offset="100%" stop-color={gradient.toColor} />
+          </linearGradient>
+        </defs>
+        <rect fill="url(#gradient)" x="0" y="0" width={size} height={size} />
+        {fileType === "svg" && text && (
+          <text
+            x="50%"
+            y="50%"
+            alignment-baseline="central"
+            dominant-baseline="central"
+            text-anchor="middle"
+            fill="#fff"
+            font-family="sans-serif"
+            font-size={(size * 0.9) / text.length}
+          >
+            {text}
+          </text>
+        )}
+      </g>
+    </svg>
+  );
+
+  if (fileType === "svg") {
+    const stream = await renderToReadableStream(avatar);
+    return new Response(stream, {
+      headers: {
+        "Content-Type": "image/svg+xml",
+        "Cache-Control": "public, max-age=604800, immutable",
+      },
+    });
+  }
+
+  return new ImageResponse(avatar, {
+    width: size,
+    height: size,
+  });
+}
