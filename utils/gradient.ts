6fa923064f0fe89ff140c059fc636573e781b8cf
Steven <steven@ceriously.com>
2024-09-30 18:45:12 -0400

    fix: djb2 algorithm

--- utils/gradient.ts
+++ utils/gradient.ts
@@ -1,15 +1,28 @@
 import color from "tinycolor2";
 
-export function djb2(str: string) {
-  let hash = 5381;
+/**
+ * This algorithm was first reported by dan bernstein in comp.lang.c.
+ * We use BigInt in JS which is similar to "unsigned long" in C.
+ * See http://www.cse.yorku.ca/~oz/hash.html
+ */
+function djb2(str: string): bigint {
+  let hash = 5381n;
   for (let i = 0; i < str.length; i++) {
-    hash = (hash << 5) + hash + str.charCodeAt(i);
+    hash = (hash << 5n) + hash + BigInt(str.charCodeAt(i));
   }
   return hash;
 }
 
+/**
+ * Since the Hue (H in HSL) is between 0 and 360, we use the remainder
+ * then convert it to a number since we no longer need BigInt precision.
+ */
+function hue(str: string): number {
+  return Number(djb2(str) % 360n);
+}
+
 export function generateGradient(username: string) {
-  const c1 = color({ h: djb2(username) % 360, s: 0.95, l: 0.5 });
+  const c1 = color({ h: hue(username), s: 0.95, l: 0.5 });
   const second = c1.triad()[1].toHexString();
 
   return {
